117f96219f273601cd7c6af0b5e0c5e93d2ec473
Roberto Cito <98057423+RMChannel@users.noreply.github.com>
2023-10-18 11:34:42 +0200

    Create 5.8.c

---
+++ Esercizi_assegnati/171023/5.8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int h,m;
+    printf("Inserisci l'orario: ");
+    scanf("%d:%d",&h,&m);
+    h=(h*60)+m;
+    if (h>=1305) printf("Non ci sono aerei disponibili\n");
+    else {
+        printf("IL primo aereo disponibile è quello delle ");
+        if (h<480) printf("8:00am ed arriva alle 10:16am\\n");
+        else if (h<583) printf("9:43am ed arriva alle 11:52am\n");
+        else if (h<679) printf("11:19am ed arriva alle 01:31pm\n");
+        else if (h<767) printf("12:47pm ed arriva alle 03:00pm\n");
+        else if (h<840) printf("02:00pm ed arriva alle 04:08pm\n");
+        else if (h<945) printf("03:45pm ed arriva alle 05:55pm\n");
+        else if (h<1140) printf("07:00pm ed arriva alle 09:20pm\n");
+        else if (h<1305) printf("09:45pm ed arriva alle 11:58pm\n");
+    }
+}
