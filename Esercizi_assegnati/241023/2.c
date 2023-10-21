891366482a4ec2f24fa9962847313dba6cf5fc63
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-10-21 19:35:22 +0200

    Aggiunto 2.c

--- Esercizi_assegnati/241023/2.c
+++ Esercizi_assegnati/241023/2.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int n=0, count=0;
+    float i=0, media=0;
+    while (n!=-1) {
+        printf("Inserisci il voto del %d° studente: ",++count);
+        scanf("%d",&n);
+        if (n>0) {
+            i++;
+            media+=n;
+        }
+    }
+    if (i>0) printf("Media: %f\nNumero di studenti passati: %1.f\n",(media/i),i);
+    else printf("Media nulla");
 }
