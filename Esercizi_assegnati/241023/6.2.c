7b5be2c5d8e098f31bc44a954f0ec8ee0526b3c0
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-11-08 11:26:24 +0100

    Commenti per 6.2 e 6.3

--- Esercizi_assegnati/241023/6.2.c
+++ Esercizi_assegnati/241023/6.2.c
@@ -14,3 +14,12 @@ int main(void) {
     }
     printf("GCD: %d\n",gcd);
 }
+
+/*
+Questo codice sfrutta un metodo alternativo rispetto a quello proposto dal libro.
+Il mio codice individua il numero più piccolo tra i 2 e viene salvato in min, una
+volta fatto ciò si avvia un ciclo while con un indice i che parte da 1, il cilo 
+assegna i a gcd nel caso in cui sia n1 sia n2 sono divisibili per i (quindi hanno resto 0).
+Il ciclo continua finché i non è uguale a min, facendo così testiamo ogni numero fino al numero più
+piccolo tra i 2, così da trovare tutti i casi possibili di divisori comuni.
+*/
--- Esercizi_assegnati/241023/6.3.c
+++ Esercizi_assegnati/241023/6.3.c
@@ -30,3 +30,15 @@ int main(void) {
     }
     printf("%d/%d\n",n1,n2);
 }
+
+/*
+Questo codice sfrutta lo stesso metodo utilizzato nell'esercizio 6.2.c
+Per gestire i negativi, vado prima di tutto a controllare se n1 o n2 lo sono,
+se lo sono allora una variabile di controllo chiamata n1n/n2n (inizializzate a 0)
+viene impostato a 1 e n1/n2 viene trasformato in positivo.
+
+Dopo aver semplificato i due numeri dividendoli per tutti i gcd trovati finché esso è maggiore di 1,
+andiamo a controllare le variabili di controllo, se sono uguali a 1 tutte e due allora non faccio niente
+(perché un numero negativo fratto un altro numero negativo, diventa positivo), oppure nel caso n1n o n2n sono 1
+i numeri vengono riportati a negativo.
+*/
